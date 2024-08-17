1960ca037cfd54712e561039c11d739359d017dd
Tino Britty <153193545+brittytino@users.noreply.github.com>
2024-08-17 12:34:39 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './Images/jdlogosvg.svg';
-import logo1 from './Components/Zoeiro.png';
 
 
 function Header() {
@@ -9,7 +8,7 @@ function Header() {
             <section>
                 <nav className="navbar navbar-expand-lg bg-body-white py-3  bg-white">
                     <div className="container-fluid">
-                        <img src={logo1} alt="website logo" />
+                        <img src={logo} alt="website logo" />
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
