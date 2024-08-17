b33f1e5e96d77df7e0089dcf931ec7f8f2564039
Tino Britty <153193545+brittytino@users.noreply.github.com>
2024-08-17 12:22:11 +0530

    Update SearchPage.js

--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -10,15 +10,15 @@ function SearchPage() {
                         <div className='col-6'>
                             <div className='row justify-content-around'>
                                 <div className='col-12'>
-                                    <h3 className='d-flex' >Search across  <span className='d-flex'> <b>‘2.8 Crore+’ </b> <aside>Products & Services</aside>  </span>   <span className='d-flex'> <b>‘3.4 Crore+‘</b> <aside>Businesses </aside> </span></h3>
+                                    <h3 className='d-flex' >Search across  <span className='d-flex'> <b>‘2.8 Lakhs+’ </b> <aside>Products & Services</aside>  </span>   <span className='d-flex'> <b>‘3.4 Lakhs+‘</b> <aside>Businesses </aside> </span></h3>
                                 </div>
                                 <div className='col-4 border pseudo-search1'>
                                     <div className=" ">
-                                        <input type="text"  placeholder='Mumbai' autofocus required />
+                                        <input type="text"  placeholder='Erode' autofocus required />
                                     </div>
                                 </div>
                                 <div className='col-7 border pseudo-search'>
-                                        <input type="text" placeholder='Mumbai' autofocus required />
+                                        <input type="text" placeholder='Erode' autofocus required />
 
                                         <i class="fa-solid fa-2x  fa-microphone"></i>
                                             <button className="fa fa-search   btn ms-3 btn-danger" type="submit"></button>
